a156c53e75d2ce478314939127987bbf7a71e025
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-05-23 22:39:19 +0530

    Update Main.ino

--- Main.ino
+++ Main.ino
@@ -1,3 +1,18 @@
+/*
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+*/
+
 #include<LiquidCrystal.h>
 #include<EEPROM.h>
 
